ed52f5e892ffba2d00c7322c83c5cf5bbda1cf5e
Sadamori Kojaku <skojaku@iu.edu>
2020-11-06 14:49:31 -0500

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from setuptools import setup, find_packages
+
+setup(name='emlens', 
+      version='0.0.1', 
+      description = 'A lightweight toolbox for understanding embedding space', 
+      author = 'Sadamori Kojaku', 
+      packages=find_packages(),
+      zip_safe=False)
