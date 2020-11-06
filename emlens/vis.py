2889c80a0fccb5536ff32db09b58eb886000ae3c
Sadamori Kojaku <skojaku@iu.edu>
2020-11-05 23:25:10 -0500

    add doc string

--- emlens/vis.py
+++ emlens/vis.py
@@ -19,6 +19,30 @@ def repel_labels(
 ):
     """
     Add text labels to the points. The position of text will be automatically adjusted to avoid overlap.
+
+    Parameters
+    ----------
+    ax : matplotlib axis
+    x : numpy.array
+    y : numpy.array
+    labels : list of str
+        Text labels.
+    color : str (Optional; Default "#4d4d4d")
+        Hex code for the color of lines
+    label_width : int (Optional; Default 30)
+        Maximum length of labels. If the text exceeds this length,
+        it is automatically folded
+    arrow_shring : int (Optional; Default 5)
+        Determine the margin betwen point and dashed line.
+        This parameter is passed to `arrowprops` of `adjust_text`
+    text_param : dict (Optional; Default {})
+        parametrs that are passed to `ax.text`
+    adjust_text_params : dict (Optional; Default {})
+        parametrs that are passed to `adjust_text``
+
+    Returns
+    ------
+    ax : matplotlib ax
     """
     txt_list = []
     for i, label in enumerate(labels):
