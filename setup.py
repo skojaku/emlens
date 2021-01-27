9944268bfb5ccadb96137ddad7721bbd36a00d65
skojaku <skojaku@iu.edu>
2021-01-26 23:48:02 -0500

    fix for read the doc

--- setup.py
+++ setup.py
@@ -4,8 +4,10 @@ from setuptools import setup, find_packages
 
 setup(name='emlens', 
       version='0.0.1', 
-
       description = 'A lightweight toolbox for understanding embedding space', 
       author = 'Sadamori Kojaku', 
       packages=find_packages(),
       zip_safe=False)
+
+if not os.getenv('READTHEDOCS'):
+    install_requires.append('faiss')
