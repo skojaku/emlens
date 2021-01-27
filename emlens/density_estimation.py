975839debe01651c3af676839ef9c1cf6dc9fe2d
Sadamori Kojaku <skojaku@iu.edu>
2021-01-26 21:36:46 -0500

    update

--- emlens/density_estimation.py
+++ emlens/density_estimation.py
@@ -1,42 +1,38 @@
+import faiss
 import numpy as np
-from joblib import Parallel, delayed
-from scipy import sparse
-from sklearn.neighbors import KernelDensity
+from scipy import sparse, special
 
 
-def estimate_pdf(locations, emb, kernel="epanechnikov", dh=None, n_jobs=30):
-    """Estimate the density of points at given locations in embedding space. A
-    kernel density estimation is used to compute the density.
+def estimate_pdf(locations, emb, C0=0.1):
+    """Estimate the density of points at given locations in embedding space
+    using the KNN desity estimator.
+
+    Reference
+    https://faculty.washington.edu/yenchic/18W_425/Lec7_knn_basis.pdf
 
     :params: locations: Location at which the density is calculated.
     :type locations: numpy.array, shape=(num_locations, dim)
     :params emb: Embedding vectors of points
     :type emb: numpy.ndarray, (num_point, dim)
-    :params kernel: type of kernel, See https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KernelDensity.html#sklearn.neighbors.KernelDensity., defaults to "epanechnikov"
-    :type kernel: str, optional
-    :params n_jobs: Number of cores., defaults to 30
-    :type n_jobs: int, optional
+    :params C: Parameter for band width. Roughly C * emb.shape[0] nearest neighbors will be used for density estimation.
+    :type C: str, optional
     :return: Density of points given by `emb` at `locations`.
     :rtype: numpy.ndarray (num_locations,)
     """
-    if dh is None:
-        dh = np.power(
-            emb.shape[0], -1.0 / (emb.shape[1] + 4)
-        )  # Silverman's rule-of-thumb rule
-    kde = KernelDensity(kernel=kernel, bandwidth=dh).fit(emb)
+    n = emb.shape[0]
+    dim = emb.shape[1]
+    k = int(np.round(C0 * np.power(n, 4 / 5)))
 
-    def est(kde, locations, ids):
-        p = kde.score_samples(locations[ids, :])
-        return p, ids
+    # Construct the knn graph
+    index = faiss.IndexFlatL2(dim)
+    index.add(emb.astype(np.float32))
+    distances, indices = index.search(locations.astype(np.float32), k=k)
 
-    chunks = np.array_split(np.arange(locations.shape[0]), n_jobs)
-    results = Parallel(n_jobs=n_jobs)(
-        delayed(est)(kde, locations, ids) for ids in chunks
-    )
-
-    ids = np.concatenate([res[1] for res in results])
-    prob_density = np.concatenate([res[0] for res in results])
-    order = np.argsort(ids)
-    ids = ids[order]
-    prob_density = prob_density[order]
-    return prob_density
+    #
+    # KNN density estimator
+    # https://faculty.washington.edu/yenchic/18W_425/Lec7_knn_basis.pdf
+    #
+    logVd = np.log(np.pi) * (dim / 2.0) - special.loggamma(dim / 2.0 + 1)
+    Rk = np.max(distances, axis=1)
+    density = np.log(k) - np.log(n) - dim * np.log(Rk) - logVd
+    return density
