5be3ec0437c30aef5d451ad7b298d49b9d833296
skojaku <skojaku@iu.edu>
2021-01-27 08:14:33 -0500

    mocking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,11 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("./../.."))
-import emlens
+
+from unittest.mock import MagicMock
+
+MOCK_MODULES = ['faiss']
+sys.modules.update((mod_name, MagicMock()) for mod_name in MOCK_MODULES)
 
 
 # -- Project information -----------------------------------------------------
@@ -28,15 +32,6 @@ release = "0.0.1"
 
 
 # -- General configuration ---------------------------------------------------
-from unittest.mock import MagicMock
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-            return Mock()
-
-MOCK_MODULES = ['faiss']
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
