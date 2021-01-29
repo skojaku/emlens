8466f92c9c3642fb7586d35d59822b15531e8076
jisungyoon <dbswlgm@gmail.com>
2021-01-29 02:56:05 -0500

    implement rog

--- emlens/metrics.py
+++ emlens/metrics.py
@@ -4,6 +4,7 @@ from joblib import Parallel, delayed
 from scipy import sparse, stats
 from sklearn import utils
 from sklearn.metrics import average_precision_score, roc_curve
+from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 
 
@@ -182,3 +183,34 @@ def pairwise_distance(emb, y):
                 D[k, ll] = d
                 D[ll, k] = d
     return D, labels
+
+def radius_of_gyration(emb, distance_function='euc'):
+    """Calculate radius of gyration -- atypicalness for sets of embedding vectors
+    For the detail, please read https://en.wikipedia.org/wiki/Radius_of_gyration
+    
+    :param emb: embedding
+    :type emb: numpy.ndarray
+    :param distance_function: only cosine distance ('cos') and eucliduan distance ('euc') are supported now.
+    :type y: str Type of distance function 
+    :return: Distance matrix D, and labels for groups
+    :rtype: numpy.ndarray, numpy.ndarray
+    
+    .. highlight:: python
+    .. code-block:: python 
+
+        >>> import emlens
+        >>> import numpy as np
+        >>> emb = np.random.randn(100, 20)
+        >>> rog = emlens.pairwise_distance(emb, 'cos')
+    """
+    
+    mean_vec = emb.mean(axis=0)
+    if distance_function == 'euc':
+        diff_emb = emb - mean_vec
+        rog = np.sqrt(np.mean(np.sum(np.power(diff_emb, 2), axis=1)))
+    elif distance_function == 'cos':
+        rog = np.sqrt(np.mean(np.power(1 - cosine_similarity(emb, [mean_vec]), 2)))
+    else:
+        raise NotImplementedError("radious of gyration function does not support distance_function: {}".format(distance_function))
+        
+    return rog
